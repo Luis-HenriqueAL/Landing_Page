04fb8e8df1868a20c939a6803db3af297b2d0092
Luis Henrique Augusto de Lima <luis.lima0302@gmail.com>
2025-03-10 21:17:23 -0300

    Criação da estrutura de pastas e arquivos
    Criação da estrutura de pastas e arquivos iniciais, além das estilizações iniciais.

---
+++ commons/header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>All In - Treinamentos</title>
+	<base href="http://localhost/landing_page/">
+	<link rel="stylesheet" type="text/css" href="resources/css/main.css">
+</head>
+<body>
+
+	<header>
+		<div class="container">
+			
+			<div class="content">
+				
+				<div class="logo">
+					<img src="resources/images/logo.png" alt="Logo">
+				</div>
+
+				<div class="lateral-menu">
+
+					<ul class="nav">
+						<li>
+							<a herf='#'>Quem sou eu</a>
+						</li>
+						<li>
+							<a herf='#'>Eventos</a>
+						</li>
+						<li>
+							<a herf='#'>Cursos</a>
+						</li>
+						<li>
+							<a herf='#'>Fale comigo</a>
+						</li>
+					</ul>
+
+					<div class="social-media">
+						<p>Siga-me:</p>
+						<a href="">Facebook</a>
+						<a href="">Instagram</a>
+						<a href="">Youtube</a>
+					</div>
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</header>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php 
+require_once 'commons/header.php';
+require_once 'views/home.php';
+require_once 'commons/header.php';
+
